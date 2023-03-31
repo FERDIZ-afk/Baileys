c34af595757d715c971e41e4f2142029879bd536
FERDIZ-afk <ferdizakyla@gmail.com>
2023-03-31 18:16:13 +0700

    update

--- src/Utils/auth-utils.ts
+++ src/Utils/auth-utils.ts
@@ -105,7 +105,14 @@ export const addTransactionCapability = (
 			dbQueriesInTransaction += 1
 			const result = await state.get(type, idsRequiringFetch)
 
-			transactionCache[type] = Object.assign(transactionCache[type] || { }, result)
+		//	transactionCache[type] = Object.assign(transactionCache[type] || { }, result)
+			if(!transactionCache[type] && result) {
+				transactionCache[type] ||= {}
+				transactionCache[type] = Object.assign(
+					transactionCache[type]!,
+					result
+				)
+			}
 		}
 	}
 
